26fc8350d3a107412ef871cacf23d5cf182f06b1
Kyle Maxwell <krmaxwell@gmail.com>
2015-04-30 20:50:47 -0500

    First cut at async HTTP based on http://matt.aimonetti.net/posts/2012/11/27/real-life-concurrency-in-go/

--- combine-turbo.go
+++ combine-turbo.go
@@ -6,8 +6,13 @@ import (
 	"os"
 )
 
-func main() {
-	fileName := "urls.txt"
+type HttpResponse struct {
+	url      string
+	response *http.Response
+	err      error
+}
+
+func getUrlList(fileName string) []string {
 	var urlList []string
 
 	f, err := os.Open(fileName)
@@ -23,7 +28,26 @@ func main() {
 		urlList = append(urlList, scanner.Text())
 	}
 
+	return urlList
+}
+
+func main() {
+	fileName := "urls.txt"
+	urlList := getUrlList(fileName)
+
+	ch := make(chan *http.Response, len(urlList))
 	for _, url := range urlList {
-		fmt.Println(url)
+		go func(url string) {
+			fmt.Printf("Fetching %s\n", url)
+			response, err := http.Get(url)
+			ch <- &HttpResponse{url, response, err}
+		}(url)
+	}
+
+	responses := make([]*HttpResponse, len(urlList))
+	responses = <-ch
+
+	for _, err := range responses {
+		fmt.Println(err)
 	}
 }
